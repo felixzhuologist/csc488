ec22b5070da76e0584b5d258266b99d65fc2a3ef
Felix Mo <felix@felixmo.com>
2017-02-17 01:30:00 -0500

    A3 starter code with some tweaks to get AST started

---
+++ a3/RUNCOMPILER.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+#  Location of directory containing  dist/compiler488.jar
+WHERE=.
+#  Compiler reads one source file from command line argument
+#  Output to standard output 
+#java -jar $WHERE/dist/compiler488.jar  $1
+java -jar $WHERE/dist/compiler488.jar -X -D a $1
+exit 0
