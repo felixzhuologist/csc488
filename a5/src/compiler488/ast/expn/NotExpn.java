6bd0dc504692032d5bab302cebc2359b3fb6a934
Evan Klein <evan.klein@hotmail.com>
2017-04-01 20:23:06 -0400

    codegen for not expn

--- a5/src/compiler488/ast/expn/NotExpn.java
+++ a5/src/compiler488/ast/expn/NotExpn.java
@@ -1,7 +1,13 @@
 package compiler488.ast.expn;
 
 import compiler488.ast.type.BooleanType;
+import compiler488.codegen.CodeGenErrorException;
+import compiler488.codegen.Utils;
+import compiler488.compiler.Main;
+import compiler488.runtime.Machine;
+import compiler488.runtime.MemoryAddressException;
 import compiler488.semantics.SemanticErrorException;
+import compiler488.semantics.Util;
 
 /**
  * Represents the boolean negation of an expression.
@@ -22,4 +28,16 @@ public class NotExpn extends UnaryExpn {
       }
       resultType = new BooleanType(lineNumber);
     }
+
+    @Override
+    public void doCodeGen() throws CodeGenErrorException {
+        try {
+            operand.doCodeGen();
+            Utils.generateNotCode();
+
+        } catch (MemoryAddressException e) {
+            throw new CodeGenErrorException(e.getMessage());
+        }
+    }
+
 }
