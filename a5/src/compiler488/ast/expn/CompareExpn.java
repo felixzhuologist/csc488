9e9cd6484e8779e9af5f1790e305cb12c5d04bc0
felixzhuologist <felix.czhu@gmail.com>
2017-04-01 20:09:59 -0400

    Add comp codegen

--- a5/src/compiler488/ast/expn/CompareExpn.java
+++ a5/src/compiler488/ast/expn/CompareExpn.java
@@ -29,4 +29,41 @@ public class CompareExpn extends BinaryExpn {
 
         this.resultType = new BooleanType(lineNumber);
     }
+
+  @Override
+  public void doCodeGen() throws CodeGenErrorException {
+    try {
+        left.doCodeGen();
+        right.doCodeGen();
+        short opcode;
+        switch (opSymbol) {
+            case "<": 
+                Machine.writeMemory(Main.codeGenAddr++, Machine.LT);
+                break;
+            case "<=": 
+                Machine.writeMemory(Main.codeGenAddr++, Machine.SWAP);
+                Machine.writeMemory(Main.codeGenAddr++, Machine.LT);
+                Util.generateNotCode();
+                break;
+            case ">":
+                Machine.writeMemory(Main.codeGenAddr++, Machine.SWAP);
+                Machine.writeMemory(Main.codeGenAddr++, Machine.LT);
+                break;
+            case ">=":
+                Machine.writeMemory(Main.codeGenAddr++, Machine.LT);
+                Util.generateNotCode();
+                break;
+            case "=":
+                Machine.writeMemory(Main.codeGenAddr++, Machine.EQ);
+            case "!=":
+                Machine.writeMemory(Main.codeGenAddr++, Machine.EQ);
+                Util.generateNotCode();
+            default:
+                throw new CodeGenErrorException("Unknown operation " + opSymbol);
+        }
+        Machine.writeMemory(Main.codeGenAddr++, opcode);
+    } catch (MemoryAddressException e) {
+      throw new CodeGenErrorException(e.getMessage());
+    }
+  }
 }
