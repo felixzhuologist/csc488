a5e18b699bb904b407f4a219d691a521ca610eeb
Evan Klein <evan.klein@hotmail.com>
2017-04-03 15:54:13 -0400

    codegen for unary minus

--- a5/src/compiler488/ast/expn/UnaryMinusExpn.java
+++ a5/src/compiler488/ast/expn/UnaryMinusExpn.java
@@ -1,6 +1,10 @@
 package compiler488.ast.expn;
 
 import compiler488.ast.type.IntegerType;
+import compiler488.codegen.CodeGenErrorException;
+import compiler488.compiler.Main;
+import compiler488.runtime.Machine;
+import compiler488.runtime.MemoryAddressException;
 import compiler488.semantics.SemanticErrorException;
 
 /**
@@ -22,4 +26,17 @@ public class UnaryMinusExpn extends UnaryExpn {
       }
       this.resultType = new IntegerType(lineNumber);
     }
+
+
+    @Override
+    public void doCodeGen() throws CodeGenErrorException {
+        try {
+            operand.doCodeGen();
+            Machine.writeMemory(Main.codeGenAddr++, Machine.NEG);
+
+        } catch (MemoryAddressException e) {
+            throw new CodeGenErrorException(e.getMessage());
+        }
+    }
+
 }
