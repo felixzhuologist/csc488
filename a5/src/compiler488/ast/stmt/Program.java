72dd6d2f51b38b98c3e9216ec64f8da7175286e0
Felix Mo <felix@felixmo.com>
2017-04-03 18:59:57 -0400

    Implement initialization of program

--- a5/src/compiler488/ast/stmt/Program.java
+++ a5/src/compiler488/ast/stmt/Program.java
@@ -3,6 +3,9 @@ package compiler488.ast.stmt;
 
 import compiler488.ast.ASTList;
 import compiler488.ast.decl.Declaration;
+import compiler488.compiler.Main;
+import compiler488.runtime.Machine;
+import compiler488.codegen.CodeGenErrorException;
 
 /**
  * Placeholder for the scope that is the entire program
@@ -13,5 +16,18 @@ public class Program extends Scope {
         super(lineNumber);
         this.declarations = declarations;
         this.statements = statements;
+    }
+    
+    @Override
+    public void doCodeGen() throws CodeGenErrorException {
+        try {
+            Machine.writeMemory(Main.codeGenAddr++, Machine.PUSHMT);
+            Machine.writeMemory(Main.codeGenAddr++, Machine.SETD);
+            Machine.writeMemory(Main.codeGenAddr++, (short)0);
+        }
+        catch (Exception e) {
+            throw new CodeGenErrorException(e.getMessage());
+        }
+        super.doCodeGen();
     }
 }
