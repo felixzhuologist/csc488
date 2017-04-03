ec9558ad08d287fb87a2db77df9081fc3c6bc942
Evan Klein <evan.klein@hotmail.com>
2017-04-03 17:57:59 -0400

    codegen for repeat stmt

--- a5/src/compiler488/ast/stmt/IfStmt.java
+++ a5/src/compiler488/ast/stmt/IfStmt.java
@@ -102,44 +102,6 @@ public class IfStmt extends Stmt {
 	public void doCodeGen() throws CodeGenErrorException {
 		try {
 			Utils.generateIfCode(condition, whenTrue, whenFalse);
-//
-//			// if (condition)
-//			condition.doCodeGen();
-//			Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
-//
-//			// save the address of the instruction so we can patch it later
-//			short labelFalseInstrAddr = Main.codeGenAddr;
-//			// push address to branch to if false, is undefined right now, will be patched later
-//			Machine.writeMemory(Main.codeGenAddr++, Machine.UNDEFINED);
-//			Machine.writeMemory(Main.codeGenAddr++, Machine.BF);
-//
-//			// if true then:
-//			whenTrue.doCodeGen();
-//			// need to push the address to branch to, so that it skips the else part
-//			//	PUSH label_after
-//			//	BR
-//			Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
-//
-//			if (whenFalse != null) {
-//				// save the address of the instruction so we can patch it later
-//				short labelAfterInstrAddr = Main.codeGenAddr;
-//				Machine.writeMemory(Main.codeGenAddr++, Machine.UNDEFINED);
-//				Machine.writeMemory(Main.codeGenAddr++, Machine.BR);
-//
-//				// the current address that we have right now is the address to branch to if the condition is false, so
-//				// we can patch this now
-//				Utils.patch(labelFalseInstrAddr, Main.codeGenAddr);
-//
-//				// now the else part
-//				whenFalse.doCodeGen();
-//
-//				// now we can patch the address of "after the if block"
-//				Utils.patch(labelAfterInstrAddr, Main.codeGenAddr);
-//			} else {
-//				// just patch the false address so it goes to after the if block if the condition is false
-//				Utils.patch(labelFalseInstrAddr, Main.codeGenAddr);
-//			}
-
 		} catch (MemoryAddressException e) {
 			throw new CodeGenErrorException(e.getMessage());
 		}
--- a5/src/compiler488/ast/stmt/RepeatUntilStmt.java
+++ a5/src/compiler488/ast/stmt/RepeatUntilStmt.java
@@ -5,6 +5,11 @@ import java.io.PrintStream;
 import compiler488.ast.Indentable;
 import compiler488.ast.expn.Expn;
 import compiler488.ast.type.BooleanType;
+import compiler488.codegen.CodeGenErrorException;
+import compiler488.codegen.Utils;
+import compiler488.compiler.Main;
+import compiler488.runtime.Machine;
+import compiler488.runtime.MemoryAddressException;
 
 /**
  * Represents a loop in which the exit condition is evaluated after each pass.
@@ -31,5 +36,38 @@ public class RepeatUntilStmt extends LoopingStmt {
 		body.printOn(out, depth + 1);
 		Indentable.printIndentOnLn(out, depth, " until "  + expn );
 
+	}
+
+	@Override
+	public void doCodeGen() throws CodeGenErrorException {
+		try {
+			// save the current address as the address of the loop label
+			short loopLabelAddr = Main.codeGenAddr;
+
+			// first the body
+			body.doCodeGen();
+
+			// repeat {...} until (expn)
+			expn.doCodeGen();
+
+			Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+			// save the address of the instruction so we can patch its value later
+			short labelAfterInstrAddr = Main.codeGenAddr;
+			// push address to branch to if false, is undefined right now, will be patched later
+			Machine.writeMemory(Main.codeGenAddr++, Machine.UNDEFINED);
+			Machine.writeMemory(Main.codeGenAddr++, Machine.BF);
+
+			// push the address of the loop label, so we branch back to it after each iteration
+			Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+			Machine.writeMemory(Main.codeGenAddr++, loopLabelAddr);
+			Machine.writeMemory(Main.codeGenAddr++, Machine.BR);
+
+
+			// now we can patch the address of "after the while block"
+			Utils.patch(labelAfterInstrAddr, Main.codeGenAddr);
+
+		} catch (MemoryAddressException e) {
+			throw new CodeGenErrorException(e.getMessage());
+		}
 	}
 }
