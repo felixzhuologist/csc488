4968832dbe0aa46e3415bd0f4610fcc4dcb92c5b
Atheed Thameem <atheedthameem@hotmail.com>
2017-04-04 11:03:44 -0400

    added codegen for readStmt, and associated test cases

--- a5/src/compiler488/ast/stmt/ReadStmt.java
+++ a5/src/compiler488/ast/stmt/ReadStmt.java
@@ -2,8 +2,13 @@ package compiler488.ast.stmt;
 
 import compiler488.ast.ASTList;
 import compiler488.ast.Readable;
+import compiler488.ast.expn.IdentExpn;
+import compiler488.ast.expn.SubsExpn;
 import compiler488.ast.type.IntegerType;
 import compiler488.ast.expn.Expn;
+import compiler488.codegen.CodeGenErrorException;
+import compiler488.compiler.Main;
+import compiler488.runtime.Machine;
 import compiler488.semantics.SemanticErrorException;
 
 import java.util.ListIterator;
@@ -43,6 +48,27 @@ public class ReadStmt extends Stmt {
 			if (!(((Expn) input).getResultType() instanceof IntegerType)) {
 				throw new SemanticErrorException("Can't read input into non integer variable");
 			}
+		}
+	}
+
+	@Override
+	public void doCodeGen() throws CodeGenErrorException {
+		ListIterator inIter = inputs.getIter();
+		try {
+			while (inIter.hasNext()) {
+				Expn input = (Expn)(inIter.next());
+				if (input instanceof IdentExpn) {
+					input.doCodeGenLHS();
+				} else if (input instanceof SubsExpn) {
+					input.doCodeGen();
+				} else {
+					throw new CodeGenErrorException("Can't read input of such an expression");
+				}
+				Machine.writeMemory(Main.codeGenAddr++, Machine.READI);
+				Machine.writeMemory(Main.codeGenAddr++, Machine.STORE);
+			}
+		} catch (Exception e) {
+			throw new CodeGenErrorException(e.getMessage());
 		}
 	}
 
