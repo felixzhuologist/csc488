af0d1fc5e95c5ac3957d581efc17c5fd64308eda
Jeffrey Deng <jeffreydeng@live.com>
2017-04-04 16:35:32 -0400

    First pass at SubsExpn

--- a5/src/compiler488/ast/expn/SubsExpn.java
+++ a5/src/compiler488/ast/expn/SubsExpn.java
@@ -6,6 +6,8 @@ import compiler488.compiler.Main;
 import compiler488.ast.type.*;
 import compiler488.semantics.Util;
 import compiler488.semantics.SemanticErrorException;
+import compiler488.codegen.CodeGenErrorException;
+import compiler488.runtime.Machine;
 
 /**
  * References to an array element variable
@@ -15,44 +17,87 @@ import compiler488.semantics.SemanticErrorException;
  */
 public class SubsExpn extends UnaryExpn implements Readable {
 
-	private String variable; // name of the array variable
+    private String variable; // name of the array variable
+    // used to calculate the memory address of the variable
+    private int lexicalLevel;   // lexical level of symbol table entry
+    private int index;          // index of symbol table entry
+    private int lb;             // lowerbound of array
 
-	public SubsExpn(Integer lineNumber, String variable, Expn operand) {
-		super(lineNumber);
-		this.variable = variable;
-		this.operand = operand;
-	}
-
-
-	/** Returns a string that represents the array subscript. */
-	@Override
-	public String toString() {
-		return (variable + "[" + operand + "]");
-	}
-
-	@Override
-	public void doSemantics() throws SemanticErrorException {
-		operand.doSemantics();
-		if (!(operand.getResultType() instanceof IntegerType)) {
-			throw new SemanticErrorException("Indexing into array with non integer type");
-		}
-
-    SymbolTableEntry entry = Main.symbolTable.getEntry(variable);
-    if (entry == null || !(entry instanceof ArraySymbol)) {
-      throw new SemanticErrorException("Reference to undeclared array variable " + variable);
+    public SubsExpn(Integer lineNumber, String variable, Expn operand) {
+        super(lineNumber);
+        this.variable = variable;
+        this.operand = operand;
+        this.lexicalLevel = -1;
+        this.index = 1;
     }
 
-    ArraySymbol arrEntry = (ArraySymbol) entry;
-    this.resultType = Util.getTypeWithLineNumber(arrEntry.getType(), lineNumber);
-	}
 
-	public String getVariable() {
-		return variable;
-	}
+    /** Returns a string that represents the array subscript. */
+    @Override
+    public String toString() {
+        return (variable + "[" + operand + "]");
+    }
 
-	public void setVariable(String variable) {
-		this.variable = variable;
-	}
+    @Override
+    public void doSemantics() throws SemanticErrorException {
+        operand.doSemantics();
+        if (!(operand.getResultType() instanceof IntegerType)) {
+            throw new SemanticErrorException("Indexing into array with non integer type");
+        }
+
+        SymbolTableEntry entry = Main.symbolTable.getEntry(variable);
+        if (entry == null || !(entry instanceof ArraySymbol)) {
+          throw new SemanticErrorException("Reference to undeclared array variable " + variable);
+        }
+
+        this.lexicalLevel = entry.getDepth();
+        this.index = entry.getIndex();
+
+        ArraySymbol arrEntry = (ArraySymbol) entry;
+        this.resultType = Util.getTypeWithLineNumber(arrEntry.getType(), lineNumber);
+        
+        this.lb = arrEntry.getLb();
+    }
+    
+    @Override
+    public void doCodeGen() throws CodeGenErrorException {  
+     
+        try {
+            Machine.writeMemory(Main.codeGenAddr++, Machine.ADDR);
+            Machine.writeMemory(Main.codeGenAddr++, (short)this.lexicalLevel);
+            Machine.writeMemory(Main.codeGenAddr++, (short)this.index);
+            
+            // This will PUSH the operand's value
+            this.operand.doCodeGen();
+            
+            if (this.lb != 1) {
+                Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+                Machine.writeMemory(Main.codeGenAddr++, (short)this.lb);
+                Machine.writeMemory(Main.codeGenAddr++, Machine.SUB);
+            }
+            
+            // Might need to multiply by sizeof(short) here
+            Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+            Machine.writeMemory(Main.codeGenAddr++, (short)2);
+            Machine.writeMemory(Main.codeGenAddr++, Machine.MUL);
+            
+            Machine.writeMemory(Main.codeGenAddr++, Machine.ADD);
+            Machine.writeMemory(Main.codeGenAddr++, Machine.LOAD);
+        }
+        catch (Exception e) {
+            System.out.println("Thrown in SubsExpn");
+            throw new CodeGenErrorException(e.getMessage());
+        }
+    }
+
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
 
 
 }
