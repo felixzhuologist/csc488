cf3ba9520db2845ebed50acde5b388869c6e6552
Felix Mo <felix@felixmo.com>
2017-04-03 20:44:13 -0400

    Reserve space for declared variables + related test

--- a5/src/compiler488/ast/stmt/Scope.java
+++ a5/src/compiler488/ast/stmt/Scope.java
@@ -8,6 +8,7 @@ import compiler488.ast.decl.Declaration;
 import compiler488.codegen.CodeGenErrorException;
 import compiler488.compiler.Main;
 import compiler488.semantics.SemanticErrorException;
+import compiler488.runtime.Machine;
 
 /**
  * Represents the declarations and instructions of a scope construct.
@@ -83,6 +84,16 @@ public class Scope extends Stmt {
 
 	@Override
 	public void doCodeGen() throws CodeGenErrorException {
+	    try {
+	        Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+	        Machine.writeMemory(Main.codeGenAddr++, Machine.UNDEFINED);
+	        Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+	        Machine.writeMemory(Main.codeGenAddr++, (short)declarations.size());
+	        Machine.writeMemory(Main.codeGenAddr++, Machine.DUPN);
+	    }
+	    catch (Exception e) {
+	        throw new CodeGenErrorException(e.getMessage());
+	    }
 		declarations.doCodeGen();
 		statements.doCodeGen();
 	}
