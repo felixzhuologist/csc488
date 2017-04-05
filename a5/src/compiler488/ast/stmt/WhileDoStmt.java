249f2a1142b65ea0787c3305c048f0f3e0c9a065
Evan Klein <evan.klein@hotmail.com>
2017-04-04 23:40:14 -0400

    codegen for while stmt

--- a5/src/compiler488/ast/stmt/WhileDoStmt.java
+++ a5/src/compiler488/ast/stmt/WhileDoStmt.java
@@ -4,7 +4,11 @@ import java.io.PrintStream;
 
 import compiler488.ast.Indentable;
 import compiler488.ast.expn.Expn;
-import compiler488.ast.type.BooleanType;
+import compiler488.codegen.CodeGenErrorException;
+import compiler488.codegen.Utils;
+import compiler488.compiler.Main;
+import compiler488.runtime.Machine;
+import compiler488.runtime.MemoryAddressException;
 
 
 /**
@@ -20,7 +24,7 @@ public class WhileDoStmt extends LoopingStmt {
 
 	/**
 	 * Print a description of the <b>while-do</b> construct.
-	 * 
+	 *
 	 * @param out
 	 *            Where to print the description.
 	 * @param depth
@@ -31,5 +35,38 @@ public class WhileDoStmt extends LoopingStmt {
 		Indentable.printIndentOnLn(out, depth, "while " + expn + " do");
 		body.printOn(out, depth + 1);
 		Indentable.printIndentOnLn(out, depth, "End while-do");
+	}
+
+
+	@Override
+	public void doCodeGen() throws CodeGenErrorException {
+		try {
+			// save the current address as the address of the loop label
+			short loopLabelAddr = Main.codeGenAddr;
+
+			// while (expn)
+			expn.doCodeGen();
+			Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+
+			// save the address of the instruction so we can patch its value later
+			short labelAfterInstrAddr = Main.codeGenAddr;
+			// push address to branch to if false, is undefined right now, will be patched later
+			Machine.writeMemory(Main.codeGenAddr++, Machine.UNDEFINED);
+			Machine.writeMemory(Main.codeGenAddr++, Machine.BF);
+
+			// now the body
+			body.doCodeGen();
+
+			// push the address of the loop label, so we branch back to it after each iteration
+			Machine.writeMemory(Main.codeGenAddr++, Machine.PUSH);
+			Machine.writeMemory(Main.codeGenAddr++, loopLabelAddr);
+			Machine.writeMemory(Main.codeGenAddr++, Machine.BR);
+
+
+			// now we can patch the address of "after the while block"
+			Utils.patch(labelAfterInstrAddr, Main.codeGenAddr);
+		} catch (MemoryAddressException e) {
+			throw new CodeGenErrorException(e.getMessage());
+		}
 	}
 }
