bd46f923bafb7bcd60f46c34d270a13ebc96b4fc
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 20:35:57 -0400

    Remove start comments and unused methods

--- a5/src/compiler488/codegen/CodeGen.java
+++ a5/src/compiler488/codegen/CodeGen.java
@@ -39,7 +39,7 @@ import compiler488.runtime.MemoryAddressException;
  *      startMSP        initial value for msp
  *      startMLP        initial value for mlp
  * </pre>
- * @author  <B> PUT YOUR NAMES HERE </B>
+ * @author  Evan Klein, Felix Zhu, Atheed Thameem, Felix Mo, Jeffrey Deng
  */
 
 public class CodeGen
@@ -62,30 +62,14 @@ public class CodeGen
      */
     public CodeGen()
     {
-        // YOUR CONSTRUCTOR GOES HERE.
+
     }
-
-    // Utility procedures used for code generation GO HERE.
-
-    /**
-     *  Additional intialization for gode generation.
-     *  Called once at the start of code generation.
-     *  May be unnecesary if constructor does everything.
-     */
 
     /** Additional initialization for Code Generation (if required) */
     void Initialize()
     {
-        /********************************************************/
-	/* Initialization code for the code generator GOES HERE */
-	/* This procedure is called once before codeGeneration  */
-	/*                                                      */
-        /********************************************************/
-
-
         return;
     }
-
 
     /**
      *  Perform any requred cleanup at the end of code generation.
@@ -95,48 +79,12 @@ public class CodeGen
     void Finalize()
             throws MemoryAddressException     // from Machine.writeMemory
     {
-        /********************************************************/
-	/* Finalization code for the code generator GOES HERE.  */
-	/*                                                      */
-	/* This procedure is called once at the end of code     */
-	/* generation                                           */
-        /********************************************************/
-
-        //  REPLACE THIS CODE WITH YOUR OWN CODE
-        //  THIS CODE generates a single HALT instruction
-        //  as an example.
         Machine.setPC( (short) 0 ) ;		/* where code to be executed begins */
         Machine.setMSP((short)  (Main.codeGenAddr + 1) );   	/* where memory stack begins */
         Machine.setMLP((short) ( Machine.memorySize -1 ) );
 					/* limit of stack */
         Machine.writeMemory((short)  Main.codeGenAddr , Machine.HALT );
 
-        return;
-    }
-
-    /** Procedure to implement code generation based on code generation
-     *  action number
-     * @param actionNumber  code generation action to perform
-     */
-    void generateCode( int actionNumber )
-    {
-        if( traceCodeGen )
-        {
-            //output the standard trace stream
-            Main.traceStream.println("CodeGen: C" +  actionNumber );
-        }
-
-        /****************************************************************/
-	/*  Code to implement the code generation actions GOES HERE     */
-	/*  This dummy code generator just prints the actionNumber      */
-	/*  In Assignment 5, you'll implement something more interesting */
-        /*                                                               */
-        /*  FEEL FREE TO ignore or replace this procedure                */
-        /****************************************************************/
-
-
-
-        System.out.println("Codegen: C" + actionNumber );
         return;
     }
 
